f7d8721932cf6d7b82ba4c19e432b30a69c225e3
ondra <o.slampa@gmail.com>
2013-06-17 11:50:32 +0200

    box responds to get_orientation

--- lib/window_process/box.ex
+++ lib/window_process/box.ex
@@ -1,5 +1,13 @@
 defmodule WindowProcess.Box do
+  require Constants
 
+  def respond(object, :get_orientation, []) do
+    o = :wxBoxSizer.getOrientation object
+    case o do
+      Constants.wxVERTICAL->:vertical
+      Constants.wxHORIZONTAL->:horizontal
+    end
+  end
   def respond(object, func, options),
     do: WindowProcess.Sizer.respond object, func, options
 
