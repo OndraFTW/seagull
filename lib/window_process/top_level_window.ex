3a75be4d89e8a336f52144e00de92a2f6c43a099
ondra <o.slampa@gmail.com>
2013-06-18 16:12:17 +0200

    get/set title

--- lib/window_process/top_level_window.ex
+++ lib/window_process/top_level_window.ex
@@ -1,5 +1,7 @@
 defmodule WindowProcess.TopLevelWindow do
 
+  def respond(object, :get_title, []), do: list_to_binary :wxTopLevelWindow.getTitle(object)
+  def respond(object, :set_title, title), do: :wxTopLevelWindow.setTitle object, binary_to_list(title)
   def respond(object, func, options), do: WindowProcess.Window.respond object, func, options
 
 end
