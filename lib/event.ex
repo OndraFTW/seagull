a4f984998ec27763030b6c655445fee11d8a6164
ondra <o.slampa@gmail.com>
2013-07-15 20:54:22 +0200

    menu events translation moved to Event.Menu

--- lib/event.ex
+++ lib/event.ex
@@ -1,34 +1,15 @@
 defmodule Event do
   
-  def translate(_id, {:wx_ref, menu_ref, :wxMenu, _}, _data, {:wxMenu, :menu_open}, window) do
-    menu=get_widget_by_wx_ref menu_ref, window
-    pid=Keyword.get menu, :pid
-    id=Keyword.get menu, :id
-    pid<-[self, id, :open]
-    true
+  def translate(wx_id, object, {:menu, id}, event, window) do
+    if not Event.Menu.translate wx_id, object, id, event, window do
+      raise :uknown_event
+    end
   end
-  def translate(_id, {:wx_ref, menu_ref, :wxMenu, _}, _data, {:wxMenu, :menu_close}, window) do
-    menu=get_widget_by_wx_ref menu_ref, window
-    pid=Keyword.get menu, :pid
-    id=Keyword.get menu, :id
-    pid<-[self, id, :close]
-    true
-  end
-  def translate(_id, {:wx_ref, menu_ref, :wxMenu, _}, _data, {:wxMenu, :menu_highlight}, window) do
-    menu=get_widget_by_wx_ref menu_ref, window
-    pid=Keyword.get menu, :pid
-    id=Keyword.get menu, :id
-    pid<-[self, id, :highlight]
-    true
-  end
-  def translate(_id, _object, _data, _event, _window) do
-    false
-  end
-
-  defp get_widget_by_wx_ref(ref, []) do
+ 
+  def get_widget_by_wx_ref(ref, []) do
     raise {:widget_not_found, ref}
   end
-  defp get_widget_by_wx_ref(ref, [{_id, widget}|tail]) do
+  def get_widget_by_wx_ref(ref, [{_id, widget}|tail]) do
     {:wx_ref, wx_ref, _, _}=Keyword.get widget, :wxobject
     if wx_ref==ref, do: widget, else: get_widget_by_wx_ref(ref, tail)
   end
--- lib/event/menu.ex
+++ lib/event/menu.ex
@@ -12,11 +12,37 @@ defmodule Event.Menu do
 
   lc {sg, wx} inlist @events do
     def react(data, unquote(sg)) do
-      :wxEvtHandler.connect Keyword.get(data, :wxobject), unquote(wx)
+      :wxEvtHandler.connect Keyword.get(data, :wxobject), unquote(wx), [userData: {:menu, Keyword.get(data, :id)}]
       true
     end
   end
 
   def react(_button, _id, _event, _pid), do: false
+
+  def translate(_wxid, _wxobject, id, {:wxMenu, :menu_open}, window) do
+    menu=Keyword.get window, id
+    pid=Keyword.get menu, :pid
+    id=Keyword.get menu, :id
+    pid<-[self, id, :open]
+    true
+  end
+  def translate(_wxid, _wxobject, id, {:wxMenu, :menu_close}, window) do
+    menu=Keyword.get window, id
+    pid=Keyword.get menu, :pid
+    id=Keyword.get menu, :id
+    pid<-[self, id, :close]
+    true
+  end
+  def translate(_wxid, _wxobject, id, {:wxMenu, :menu_highlight}, window) do
+    menu=Keyword.get window, id
+    pid=Keyword.get menu, :pid
+    id=Keyword.get menu, :id
+    pid<-[self, id, :highlight]
+    true
+  end
+  def translate(_wx_id, _object, _id, _event, _window) do
+    false
+  end
+
 
 end
