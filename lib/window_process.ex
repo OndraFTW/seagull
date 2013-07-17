59cdfef0327bfb2ba74c0d0818a395d821b0dcaa
ondra <o.slampa@gmail.com>
2013-07-17 14:56:47 +0200

    WindowProcess.response generated by metaprogramming

--- lib/window_process.ex
+++ lib/window_process.ex
@@ -47,11 +47,17 @@ defmodule WindowProcess do
     window
   end
 
+  @classes [
+    button: WindowProcess.Button,
+    frame: WindowProcess.Frame,
+    box: WindowProcess.Box,
+    text_box: WindowProcess.TextBox
+  ]
+
   # Returns response to message func for object id with params in to pid.
-  defp respond(:button, object, func, params), do: WindowProcess.Button.respond object, func, params
-  defp respond(:frame, object, func, params), do: WindowProcess.Frame.respond object, func, params
-  defp respond(:box, object, func, params), do: WindowProcess.Box.respond object, func, params
-  defp respond(:text_box, object, func, params), do: WindowProcess.TextBox.respond object, func, params
+  lc {type, class} inlist @classes do
+    defp respond(unquote(type), object, func, params), do: unquote(class).respond object, func, params
+  end
   defp respond(type, _object, _func, _params), do: raise {:uknown_type, type}
 
 end
