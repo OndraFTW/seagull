0e04e09687fabeff4015dadeed0b524c475a1ad6
Ondřej Šlampa <o.slampa@gmail.com>
2013-10-29 17:28:06 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Seagull.Mixfile do
   def project do
     [ app: :seagull,
       version: "0.0.1",
-      elixir: "~> 0.10.4-dev"
+      elixir: "~> 0.10.4-dev",
       deps: deps ]
   end
 
